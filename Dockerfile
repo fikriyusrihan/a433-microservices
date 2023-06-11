a3ecee8d386af241aca4ee87b0edeaea51301adc
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2023-06-11 10:50:33 +0700

    add comments

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
+# Menggunakan base image node versi 18-alpine
 FROM node:18-alpine
 
+# Membuat sebuah working directory di /src
 WORKDIR /src
 
+# Menyalin package.json dan package-lock.json ke dalam working directory
 COPY package*.json ./
 
+# Menjalankan perintah npm ci untuk menginstall dependency dengan clean install
 RUN npm ci
 
+# Menyalin semua file dengan ekstensi .js ke dalam working directory
 COPY ./*.js ./
 
+# Menjalankan perintah node index.js untuk menjalankan aplikasi
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -13,9 +13,13 @@ var channel, connection;
 connectToQueue();
 
 async function connectToQueue() {
+    // Membuat koneksi dengan AMQP server yaitu RabbitMQ
     connection = await amqp.connect(amqpServer);
+
+    // Membuat channel untuk berkomunikasi dengan AMQP server
     channel = await connection.createChannel();
     try {
+        // Membuat queue dengan nama "order"
         const queue = "order";
         await channel.assertQueue(queue);
         console.log("Connected to the queue!")
@@ -24,16 +28,25 @@ async function connectToQueue() {
     }
 }
 
+// Membuat endpoint untuk membuat order
 app.post("/order", (req, res) => {
+    // Mengambil data order dari request body
     const { order } = req.body;
+
+    // Memanggil fungsi createOrder untuk mengirim pesan ke queue "order"
     createOrder(order);
+
+    // Mengembalikan response dengan data order
     res.send(order);
 });
 
 const createOrder = async order => {
+    // Mengirim pesan ke queue "order"
     const queue = "order";
     await channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)));
     console.log("Order succesfully created!")
+
+    // Ketika menerima sinyal SIGINT, maka tutup koneksi dengan AMQP server
     process.once('SIGINT', async () => { 
         console.log('got sigint, closing connection');
         await channel.close();
