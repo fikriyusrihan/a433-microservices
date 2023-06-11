762edcceb9f3a9c5ec9dfc075d33aa2e983ea6c8
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2023-06-11 10:45:54 +0700

    add comment

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
+# Menggunakan base image node:18-alpine dari Docker Hub
 FROM node:18-alpine
 
+# Membuat working directory /src
 WORKDIR /src
 
+# Memindahkan file package.json dan package-lock.json ke working directory
 COPY package*.json ./
 
+# Menjalankan perintah npm ci untuk menginstall dependency dengan clean install
 RUN npm ci
 
+# Memindahkan seluruh berkas dengan ekstensi .js ke working directory
 COPY ./*.js ./
 
+# Menjalankan perintah node index.js untuk menjalankan aplikasi
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const bp = require("body-parser");
 
+// Import amqplib untuk berinteraksi dengan protokol AMQP
 const amqp = require("amqplib");
 const amqpServer = process.env.AMQP_URL;
 var channel, connection;
@@ -13,12 +14,23 @@ connectToQueue();
 
 async function connectToQueue() {
     try {
+        // Membuat koneksi dengan AMQP server yaitu RabbitMQ
         connection = await amqp.connect(amqpServer);
+
+        // Membuat channel untuk berkomunikasi
         channel = await connection.createChannel();
+
+        // Membuat queue dengan nama "order"
         await channel.assertQueue("order");
+
+        // Mengonsumsi queue "order"
         channel.consume("order", data => {
+
+            // Setiap ada pesan masuk, maka akan di-print ke console
             console.log(`Order received: ${Buffer.from(data.content)}`);
             console.log("** Will be shipped soon! **\n")
+
+            // Mengirimkan ACK ke RabbitMQ untuk memberitahu bahwa pesan sudah diterima
             channel.ack(data);
         });
     } catch (ex) {
