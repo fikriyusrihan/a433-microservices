742b54a90bc052203ca7cd8e99ba40ae82bfa103
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2023-06-11 09:58:20 +0700

    build order-service image

---
+++ build_push_order_service.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Build order service image using Dockerfile in current directory
+docker build -t fikriyusrihan/order-service:latest .
+
+# Tag order service image to be pushed to GitHub Container Registry
+docker tag fikriyusrihan/order-service:latest ghcr.io/fikriyusrihan/order-service:latest
+
+# Login to GitHub Container Registry using GitHub Personal Access Token
+echo "$CR_PAT" | docker login ghcr.io -u fikriyusrihan --password-stdin
+
+# Push order service image to GitHub Container Registry
+docker push ghcr.io/fikriyusrihan/order-service:latest
